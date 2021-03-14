652856a7bf51eaac4df3a33917ff02f50b9bab04
Casper <133casper@gmail.com>
2021-03-14 17:40:29 +0300

    Refactor server configuration

--- src/main/models/entities/enums/AccountType.ts
+++ src/main/models/entities/enums/AccountType.ts
@@ -1,9 +1,5 @@
-// TODO: Remove disabling ESLint rules after adding functionality
-// eslint-disable-next-line no-unused-vars
 enum AccountType {
-  // eslint-disable-next-line no-unused-vars
   PERMANENT_ACCOUNT = 'PERMANENT_ACCOUNT',
-  // eslint-disable-next-line no-unused-vars
   TEMPORARY_ACCOUNT = 'TEMPORARY_ACCOUNT',
 }
 
--- src/main/models/entities/enums/AvatarMediaType.ts
+++ src/main/models/entities/enums/AvatarMediaType.ts
@@ -1,9 +1,5 @@
-// TODO: Remove disabling ESLint rules after adding functionality
-// eslint-disable-next-line no-unused-vars
 enum AvatarMediaType {
-  // eslint-disable-next-line no-unused-vars
   PNG = 'PNG',
-  // eslint-disable-next-line no-unused-vars
   JPG = 'JPG',
 }
 
