4615c4a89216f672c95433e2819ef48e12c8f673
Jackie-Dou <57232952+Jackie-Dou@users.noreply.github.com>
2021-05-15 13:55:01 +0300

    MNPL-14 | Implement creating and deleting lobby functionality (#7)
    * Create LobbyService and LobbyController
    
    * MNPL-14 | Add participation is lobby check, add lobby creator
    
    * MNPL-14 | Delete lobby
    
    * MNPL-14 | Delete lobby
    
    * MNPL-14 | Pick out checks
    
    * MNPL-14 | Lobby checks are by id
    
    * MNPL-14 | Lobby exists and User exists checks rewrite
    
    * Fix checks naming and yarn installation
    
    * MNPL-14 | Fix checks names and yarn dependency again

---
+++ src/main/controllers/LobbyController.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Post,
+  Request,
+  Route,
+  Security,
+  Tags,
+  Delete,
+  Path,
+} from 'tsoa';
+import { StatusCodes } from 'http-status-codes';
+import LobbyService from '../services/LobbyService';
+import LobbyCreationResponseDto from '../models/responses/LobbyCreationResponseDto';
+import { RequestWithUser } from '../security/JwtAuthMiddleware';
+
+@Route('/api/v1/lobbies')
+@Tags('Lobby Controller')
+// eslint-disable-next-line import/prefer-default-export
+export class LobbyController extends Controller {
+  private readonly lobbyService;
+
+  constructor() {
+    super();
+    this.lobbyService = LobbyService;
+  }
+
+  @Post()
+  @Security('JWT')
+  public async registerLobby(
+    @Request() request: RequestWithUser
+  ): Promise<LobbyCreationResponseDto> {
+    return this.lobbyService.createLobby(request.user.id);
+  }
+
+  @Delete('{lobbyId}')
+  @Security('JWT')
+  public async dissolveLobby(
+    @Path() lobbyId: number,
+    @Request() request: RequestWithUser
+  ): Promise<void> {
+    await this.lobbyService.deleteLobby(lobbyId, request.user.id);
+    this.setStatus(StatusCodes.OK);
+  }
+}
---
+++ src/main/services/LobbyService.ts
@@ -0,0 +1,89 @@
+import { getRepository } from 'typeorm';
+import Lobby from '../models/entities/Lobby';
+import LobbyParticipant from '../models/entities/LobbyParticipant';
+import LobbyStatus from '../models/entities/enums/LobbyStatus';
+import logger from '../config/logger';
+import LobbyCreationResponseDto from '../models/responses/LobbyCreationResponseDto';
+import ClientError from '../models/error/ClientError';
+import UserService from '../services/UserService';
+
+const LOBBY_DOES_NOT_EXIST = 'Лобби не существует';
+const USER_IS_IN_LOBBY = 'Вы уже являетесь участником лобби';
+const NOT_AN_OWNER = 'Вы не являетесь создателем лобби';
+const LOBBY_CREATOR_DOES_NOT_EXIST = 'Создать лобби не определён';
+
+async function isLobbyParticipant(userId: number): Promise<boolean> {
+  const lobbyParticipant = await getRepository(LobbyParticipant).findOne({
+    user: { id: userId },
+  });
+  return lobbyParticipant !== undefined;
+}
+
+async function checkThatUserIsNotLobbyParticipant(userId: number) {
+  if (await isLobbyParticipant(userId)) {
+    throw new ClientError(USER_IS_IN_LOBBY);
+  }
+}
+
+async function isLobbyCreator(
+  userId: number,
+  lobbyId: number
+): Promise<boolean> {
+  const lobbyCreator = await getRepository(LobbyParticipant).findOne({
+    lobby: { id: lobbyId },
+    isCreator: true,
+  });
+  if (lobbyCreator === undefined) {
+    throw new ClientError(LOBBY_CREATOR_DOES_NOT_EXIST);
+  }
+  return userId === lobbyCreator.user.id;
+}
+
+async function checkThatUserIsLobbyCreator(userId: number, lobbyId: number) {
+  if (!(await isLobbyCreator(userId, lobbyId))) {
+    throw new ClientError(NOT_AN_OWNER);
+  }
+}
+
+async function getLobbyByIdIfExists(lobbyId: number): Promise<Lobby> {
+  const lobby = await getRepository(Lobby).findOne(lobbyId);
+  if (lobby === undefined) {
+    throw new ClientError(LOBBY_DOES_NOT_EXIST);
+  }
+  return lobby;
+}
+
+export default {
+  async createLobby(userId: number): Promise<LobbyCreationResponseDto> {
+    const user = await UserService.getUserByIdIfExists(userId);
+    await checkThatUserIsNotLobbyParticipant(userId);
+
+    let lobby = new Lobby();
+    lobby.status = LobbyStatus.WAITING_FOR_PLAYERS;
+    lobby = await getRepository(Lobby).save(lobby);
+    logger.info(`Created lobby with id '${lobby.id}'.`);
+
+    let lobbyCreator = new LobbyParticipant();
+    lobbyCreator.user = user;
+    lobbyCreator.isCreator = true;
+    lobbyCreator.isReady = true;
+    lobbyCreator.lobby = lobby;
+    lobbyCreator = await getRepository(LobbyParticipant).save(lobbyCreator);
+    logger.info(
+      `Created lobby participant '${user.nickname}' with id '${user.id}', role - creator.`
+    );
+
+    return {
+      lobbyId: lobby.id,
+    };
+  },
+
+  async deleteLobby(lobbyId: number, userId: number): Promise<void> {
+    const user = await UserService.getUserByIdIfExists(userId);
+    const lobby = await getLobbyByIdIfExists(lobbyId);
+    await checkThatUserIsLobbyCreator(user.id, lobby.id);
+
+    logger.info(`Delete lobby '${lobby.id}'`);
+    await getRepository(Lobby).delete(lobby.id);
+  },
+};
